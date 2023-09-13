f2245c6e444270d2865aacde8bd95d0889b6c2f8
Chris <tynkastya@gmail.com>
2023-09-13 09:43:10 +0300

    added solution

--- src/main/java/org/example/task_5/Main.java
+++ src/main/java/org/example/task_5/Main.java
@@ -33,6 +33,5 @@ public class Main {
         System.out.println(productService.findProductWithSmallestPrice());
         System.out.println(productService.findProductWithNameBeginsWithC());
         System.out.println(productService.findAllProductsPriceFromUSA());
-
     }
 }
